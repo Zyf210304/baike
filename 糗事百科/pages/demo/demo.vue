12b2426e13028337cc7c2764cd980cbdd1a4d172
张亚飞 <672233580@qq.com>
2020-10-13 17:54:37 +0800

    Initial commit

---
+++ 糗事百科/pages/demo/demo.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view class="view-box animated " hover-class="view-box-hover rubberBand" hover-stay-time="2000">
+			view
+		</view>
+		
+		<text :selectable="true">
+			这&nbsp;&nbsp;&nbsp;是text\n第二行
+		</text>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.view-box {
+	width: 200upx;
+	height: 200upx;
+	background-color: #007AFF;
+	color: #FFFFFF;
+	margin: 100upx;
+}
+.view-box-hover{
+	background-color: #FF3333;
+}
+</style>
